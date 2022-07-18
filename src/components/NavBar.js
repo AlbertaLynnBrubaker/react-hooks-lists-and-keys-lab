55737571b3856dadbe1246bc4ea7c2108349f2ca
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-18 16:53:17 -0500

    commit tree 292ce64249ee0236fa0729f16c16ee9325061b84

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const renderLinks = (links) => {
+    return links.map(link => <a key={link} href={`#${link}`}>{link}</a>)
+  }
+
+  return <nav>{renderLinks(links)}</nav>;
 }
 
 export default NavBar;
